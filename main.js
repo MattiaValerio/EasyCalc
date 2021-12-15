1fcf0c8b283f589ef769076780e0cd9523e660ca
Unknown0wl <88095098+Unknown0wl@users.noreply.github.com>
2021-12-15 23:09:22 +0100

    wd

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 //------------------------------------------------------------
 //------------------------EasyCalc----------------------------
 //------------------------------------------------------------
-function somma(){
+function somma(n1, n2){
   let empty = '';
   let n1 = Number(document.getElementById("n1").value);
   let n2 = Number(document.getElementById("n2").value);
