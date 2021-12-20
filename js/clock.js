aaa5e151d8fc4220f0ea7f2ed92957a3a26cae9a
Unknown0wl <88095098+Unknown0wl@users.noreply.github.com>
2021-12-20 18:38:41 +0100

    clock page

--- js/clock.js
+++ js/clock.js
@@ -1,7 +1,22 @@
+const monthNames = ["Gennaio", "Febbraio", "Marzo", "Aprile", "Maggio", "Giugno",
+                    "Luglio", "Agosto", "Settembre", "Ottobre", "Novembre", "Dicembre"
+                ];
+
 let ck = document.getElementById("clock");
 let data = new Date;
-let ora = data.getHours();
 let minuti = data.getMinutes();
 let secondi = data.getSeconds();
+let ora = data.getHours();
+let giorno = data.getDate();
+let mese = monthNames[data.getMonth()];
+let anno = data.getFullYear();
+
 let clock = ora+':'+minuti+':'+secondi;
+let calendario = giorno +' '+mese+' '+anno
+
+console.log(clock)
+console.log(calendario)
+
+document.getElementById("cal").innerHTML= calendario;
+document.getElementById("clock").innerHTML= clock;
 
