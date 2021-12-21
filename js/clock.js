916be96f69463f8931cd8bb360c130bf31eb9d92
Unknown0wl <88095098+Unknown0wl@users.noreply.github.com>
2021-12-21 11:02:48 +0100

    hjklò

--- js/clock.js
+++ js/clock.js
@@ -2,21 +2,45 @@ const monthNames = ["Gennaio", "Febbraio", "Marzo", "Aprile", "Maggio", "Giugno"
                     "Luglio", "Agosto", "Settembre", "Ottobre", "Novembre", "Dicembre"
                 ];
 
-let ck = document.getElementById("clock");
-let data = new Date;
-let minuti = data.getMinutes();
-let secondi = data.getSeconds();
-let ora = data.getHours();
-let giorno = data.getDate();
-let mese = monthNames[data.getMonth()];
-let anno = data.getFullYear();
+function clock(){
+    let data = new Date;
+    let minuti = data.getMinutes();
+    let secondi = data.getSeconds();
+    let ora = data.getHours();
+    let giorno = data.getDate();
+    let mese = monthNames[data.getMonth()];
+    let anno = data.getFullYear();
 
-let clock = ora+':'+minuti+':'+secondi;
-let calendario = giorno +' '+mese+' '+anno
+    //Aggiungo ai secondi e minuti < 9 lo 0 davanti
+    if(secondi<=9){
+        secondi = `0${secondi}`
+    }
 
-console.log(clock)
-console.log(calendario)
+    let clock = ora+':'+minuti+':'+secondi;
+    let calendario = giorno +' '+mese+' '+anno
 
-document.getElementById("cal").innerHTML= calendario;
-document.getElementById("clock").innerHTML= clock;
+    document.getElementById("clock").innerHTML= clock;
+    document.getElementById("cal").innerHTML= calendario;
+    window.setTimeout("clock()", 1000);
+};
 
+document.innerHTML= clock();
+
+
+//----------------------------------------------------
+//--------------------ANIMAZIONI----------------------
+//----------------------------------------------------
+
+gsap.from(".orologio",{
+    scale:0.5
+})
+
+gsap.from(".calendario",{
+    scale:0,
+    y:-10
+})
+
+gsap.from(".homebtn",{
+    y:-60,
+    duration:0.5
+})
